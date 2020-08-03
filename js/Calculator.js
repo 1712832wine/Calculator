49d4a40388dc17b668962d42a26a7c599ba3dc5f
NGUYỄN HỬU TRÍ <35862674+1712832wine@users.noreply.github.com>
2020-08-03 19:31:07 +0700

    Version 1.0.3

--- js/Calculator.js
+++ js/Calculator.js
@@ -1,17 +1,43 @@
+var reset = 'false';
+
+function Reset_0() {
+    if (reset == 'true') {
+        var result = document.getElementById("result");
+        result.value = "0";
+        reset = 'false';
+    }
+}
+
+function Error() {
+    var result = document.getElementById("result");
+    result.value = "ERROR"
+    reset = 'true';
+}
+
 function ClickButton(obj) {
+    Reset_0();
     var data = obj.innerHTML;
     var result = document.getElementById("result");
     if (data != "AC" && data != "DEL" && data != "=") {
-        result.value += data;
-    } else if (data == "AC") {
-        result.value = "0";
-    } else if (data == "DEL") {
-        if (result.value != "0") {
-            result.value = result.value.substring(0, result.value.length - 1);
+        if (result.value == "0") {
+            result.value = "";
         }
-    } else if (data == "=") {
-        result.value = eval(result.value);
-        if (onkeypress()) result.value = "0";
+        if (data == 'x') result.value += '*'; //MULTIPLY
+        else result.value += data;
+    } else if (data == "AC") { //BUTTON AC
+        result.value = "0";
+    } else if (data == "DEL") { //BUTTON DEL
+        if (result.value.length > 1) {
+            result.value = result.value.substring(0, result.value.length - 1);
+        } else {
+            result.value = "0";
+        }
+    } else if (data == "=") { //BUTTON =
+        try {
+            result.value = eval(result.value);
+            reset = 'true';
+        } catch (error) {
+            Error()
+        }
     }
-
 }
